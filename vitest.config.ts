a685c2934de6738d7096f21fe7905cd692b5ade1
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-10-23 09:49:35 +0000

    vitestのbrowser modeでテストを行うように対応

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,21 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      browser: {
+        enabled: true,
+        provider: playwright(),
+        // https://vitest.dev/guide/browser/playwright
+        instances: [{ browser: 'chromium' }],
+        headless: true,
+      },
     },
   }),
 )
