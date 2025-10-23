808b6cc13a7feaec5148c7899455b6b9465e9406
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-10-23 15:01:20 +0000

    画像の高さをgetCardImageSizeから取ってくるように変更

--- src/components/ImageBox.vue
+++ src/components/ImageBox.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import Card from '@/models/card.ts'
 import blankImageUrl from '@/assets/blank.jpg'
+import { getCardImageSize } from '@/utils/card'
 
 interface Props {
   card?: Card
 }
 
 const props = defineProps<Props>()
+const imageSize = getCardImageSize('xs')
 </script>
 
 <template>
   <img
     v-bind:src="props.card?.imageUrl('xs') ?? blankImageUrl"
     v-bind:alt="props.card?.name ?? 'blank'"
-    width="100"
-    height="100"
+    v-bind:width="imageSize.width"
+    v-bind:height="imageSize.height"
   />
 </template>
--- src/components/__tests__/ImageBox.spec.ts
+++ src/components/__tests__/ImageBox.spec.ts
@@ -4,6 +4,8 @@ import ImageBox from '@/components/ImageBox.vue'
 import Card from '@/models/card.ts'
 import { render } from 'vitest-browser-vue'
 
+import blankImageUrl from '@/assets/blank.jpg'
+
 describe('ImageBox', () => {
   it('カードの内容が反映されているか', () => {
     const card = new Card(1000101, '島村卯月', 0, 0, '0dabb79ff64691111a0abae2ffed01ce')
@@ -11,5 +13,14 @@ describe('ImageBox', () => {
     const image = screen.baseElement.querySelector('img')
     expect(image?.src).toBe('https://example.com/card/xs/0dabb79ff64691111a0abae2ffed01ce.jpg')
     expect(image?.alt).toBe('島村卯月')
+    expect(image?.width).toBe(100)
+    expect(image?.height).toBe(100)
+  })
+
+  it('カードがない場合はブランク画像が表示されるか', () => {
+    const screen = render(ImageBox)
+    const image = screen.baseElement.querySelector('img')
+    expect(image?.src).toBe(new URL(blankImageUrl, location.href).href)
+    expect(image?.alt).toBe('blank')
   })
 })
