93d63615a3af838bdd90777af8e105043f07066a
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-11-09 13:21:03 +0000

    useTargetCardのテストコード追加

--- src/composables/targetCard.ts
+++ src/composables/targetCard.ts
@@ -2,18 +2,18 @@ import { ref, computed, type Ref } from 'vue'
 import Card from '@/models/card.ts'
 
 export function useTargetCard(cardList: Ref<Array<Card | undefined>>) {
-  const targetCardIndex = ref<number | null>(null)
+  const targetCardIndex = ref<number | undefined>(undefined)
   const targetCard = computed<Card | undefined>({
     get() {
       const index = targetCardIndex.value
-      if (index === null) {
+      if (index === undefined) {
         return undefined
       }
       return cardList.value[index] ?? undefined
     },
     set(card: Card | undefined) {
       const index = targetCardIndex.value
-      if (index === null) {
+      if (index === undefined) {
         return
       }
       cardList.value[index] = card
