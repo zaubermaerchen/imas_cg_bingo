edae0fd331257e55c0c13a93f06ee0434042f704
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-11-14 14:41:15 +0000

    設定項目をモーダル化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 import { VueFinalModal } from 'vue-final-modal'
+
 import BingoArea from '@/components/BingoArea.vue'
 import ResultModal from '@/components/ResultModal.vue'
+import SettingModal from '@/components/SettingModal.vue'
 import Card, { type CardSize } from '@/models/card.ts'
 
 const row = ref(3)
@@ -19,48 +21,27 @@ watch([row, column], () => {
   }
 })
 
-const visibleResultModal = ref(false)
+const isVisibleResultModal = ref(false)
 const showResultModal = () => {
-  visibleResultModal.value = true
+  isVisibleResultModal.value = true
+}
+
+const isVisibleSettingModal = ref(false)
+const showSettingModal = () => {
+  isVisibleSettingModal.value = true
 }
 </script>
 
 <template>
   <main>
-    <el-form label-position="right" label-width="100px" style="width: 300px">
-      <el-form-item label="ビンゴ枠数">
-        <el-col v-bind:span="11">
-          <el-select v-model.number="row" name="row">
-            <el-option v-for="n in 10" v-bind:key="n" v-bind:value="n" v-bind:label="`${n}行`" />
-          </el-select>
-        </el-col>
-        <el-col v-bind:span="2" style="text-align: center"> x </el-col>
-        <el-col v-bind:span="11">
-          <el-select v-model.number="column" name="column">
-            <el-option v-for="n in 10" v-bind:key="n" v-bind:value="n" v-bind:label="`${n}列`" />
-          </el-select>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="カードサイズ">
-        <el-select v-model="cardSize" name="card_size">
-          <el-option value="s" label="Sサイズ" />
-          <el-option value="m" label="Mサイズ" />
-          <el-option value="m2" label="M2サイズ" />
-          <el-option value="l" label="Lサイズ" />
-          <el-option value="l_noframe" label="Lサイズ(SR枠無し)" />
-          <el-option value="xs" label="正方形" />
-          <el-option value="ls" label="短冊" />
-        </el-select>
-      </el-form-item>
-    </el-form>
-
     <BingoArea v-bind:row="row" v-bind:column="column" v-model="cardList" />
     <el-button type="primary" v-on:click="showResultModal">画像生成</el-button>
+    <el-button type="info" v-on:click="showSettingModal">設定</el-button>
   </main>
 
   <VueFinalModal
     class="modal"
-    v-model="visibleResultModal"
+    v-model="isVisibleResultModal"
     v-bind::click-to-close="true"
     v-bind:esc-to-close="true"
   >
@@ -70,5 +51,14 @@ const showResultModal = () => {
       v-bind:column="column"
       v-bind:size="cardSize"
     />
+  </VueFinalModal>
+
+  <VueFinalModal
+    class="modal"
+    v-model="isVisibleSettingModal"
+    v-bind::click-to-close="true"
+    v-bind:esc-to-close="true"
+  >
+    <SettingModal v-model:row="row" v-model:column="column" v-model:cardSize="cardSize" />
   </VueFinalModal>
 </template>
---
+++ src/components/SettingModal.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { type CardSize } from '@/models/card.ts'
+
+const row = defineModel<number>('row', { required: true })
+const column = defineModel<number>('column', { required: true })
+const cardSize = defineModel<CardSize>('cardSize', { required: true })
+</script>
+
+<template>
+  <section class="setting-modal">
+    <el-form label-position="right" label-width="100px">
+      <el-form-item label="ビンゴ枠数">
+        <el-col v-bind:span="11">
+          <el-select v-model.number="row" name="row">
+            <el-option v-for="n in 10" v-bind:key="n" v-bind:value="n" v-bind:label="`${n}行`" />
+          </el-select>
+        </el-col>
+        <el-col v-bind:span="2" style="text-align: center"> x </el-col>
+        <el-col v-bind:span="11">
+          <el-select v-model.number="column" name="column">
+            <el-option v-for="n in 10" v-bind:key="n" v-bind:value="n" v-bind:label="`${n}列`" />
+          </el-select>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="カードサイズ">
+        <el-select v-model="cardSize" name="card_size">
+          <el-option value="s" label="Sサイズ" />
+          <el-option value="m" label="Mサイズ" />
+          <el-option value="m2" label="M2サイズ" />
+          <el-option value="l" label="Lサイズ" />
+          <el-option value="l_noframe" label="Lサイズ(SR枠無し)" />
+          <el-option value="xs" label="正方形" />
+          <el-option value="ls" label="短冊" />
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </section>
+</template>
+
+<style>
+section.setting-modal {
+  padding: 2em;
+  color: black;
+  background-color: white;
+  width: 500px;
+}
+</style>
